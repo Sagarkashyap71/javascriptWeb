b0a006a630bdef2116dcb08216cf9071484c3263
Sagar <ssji2006@gmail.com>
2026-01-19 17:28:41 +0530

    Initial commit

---
+++ 1stclass.js
@@ -0,0 +1,114 @@
+// let obj={
+//     name:"Sagar",
+//     Adress:"Yamunanagar",
+//     Phone:12344567554
+// }
+// console.log(obj)
+
+// console.log(document)
+
+// const para=document.querySelector("p")
+// const p1=document.getElementById('id1')
+// const p2=document.getElementsByClassName('class')
+
+// p1.style.color='green'
+
+// p1.textContent="This is updated hello"
+
+// console.log("Baliyan")
+
+// console.log(para)
+// console.log(p1)
+
+
+
+// const para=document.getElementsByClassName('c')
+// para[0].textContent="This is updated class"
+// para[1].style.color='green'                                                                                                                                           en"
+// console.log(para)
+
+// const para=document.getElementsByClassName('c')
+// para[0].textContent="This is updated class"
+// para[1].style.color="Green"
+// console.log(para)
+
+
+// const id2=document.getElementById('id1')
+// console.log(id2)
+
+// const para=document.querySelector('#id1')
+// para.textContent="This is first class that changed its content using queryselector"
+// para.style.color="green"
+// console.log(para)
+
+
+// let arr=[23,53,124,43,23]
+// arr.forEach((ele)=>console.log(ele))
+
+
+// second class  12-01-2026
+
+
+
+// const para=document.querySelectorAll('.c')
+// para[0].textcontent=('This is updated text')
+// para[1].style.color="green"
+// console.log(para)
+// para.forEach((ele)=>console.log(ele.textcontent))
+
+// const para=document.querySelectorAll('.c')
+// para[0].innertext=('This is updated text by innertext')  // innertext does not works on hidden text
+// para[0].textContent=('This is updated text by textcontent')
+// para[1].style.color="green"
+// para[2].innerHTML="<div><b>Hello World</b></div>"
+// console.log(para)
+
+
+// third class  16-01-2026
+
+// const button1= document.querySelector('#btn1')
+// const button2= document.querySelector('#btn2')
+// // button.classList.add('btn')                     // makes css properties in javascript during runtime
+
+// button.classList.add('btn')                        // add css properties
+// // button.classList.remove('btn')                  
+// // button.classList.toggle('btn')
+
+// function message(){
+//     alert('You have clicked the button')
+// }
+
+// button1.addEventListener('click',message)
+
+// button2.addEventListener('click',message){
+//     button1.removeEventListener('click')
+// }
+
+
+// const button= document.querySelector('#btn1')
+// button.addEventListener('keydown',function(event){
+//     console.log(event.key)
+// })
+  
+
+
+// forth class 19-01-2026
+
+function handlesubmit(event){
+    event.preventDefault()      
+    console.log("Form Submitted")
+}
+
+const form= document.querySelector("form")
+form.addEventListener("submit", handlesubmit)
+
+
+// add, commit, push
+
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Sagarkashyap71/javascriptWeb.git
+// git push -u origin main
---
+++ practice.js
@@ -0,0 +1,53 @@
+// let obj= {
+//     name: 'Vasudev',
+//     adress: 'Yamunanagar',
+//     phoneNo: "I don't know"
+// }
+// console.log(obj)
+
+
+// console.log(document)
+
+// const para= document.getElementById("para1")
+// console.log(para)
+// para.style.color='red'
+// para.textContent="updated text"
+
+// const para2= document.getElementsByClassName("pclass")
+// console.log(para2)
+// para2[0].style.color="blue"
+// para2[1].textContent="paragraph 1 of class updated"
+
+// const para3= document.querySelectorAll(".pclass")
+// console.log(para3)
+
+// const para4= document.querySelector(".pclass")
+// console.log(para4)
+
+// let array= [1,2,3,4,5]
+// array.forEach((element)=>(console.log(element)))
+
+// const para5= document.querySelectorAll(".pclass")
+// para5[0].textContent="Vasudev"
+
+// const para6= document.getElementsByClassName("pclass")
+// para6[0].innerHTML= "hello world"
+// para6[0].style.color="red"
+
+// const but1= document.querySelectorAll(".bclass")
+// but1[0].classList.add("bclass")
+// but1[0].classList.remove("bclass")
+// but1[0].classList.toggle("bclass")
+// function messagebut1(){
+//     alert("Submitted Successfully !")
+// }
+// but1[0].addEventListener("click",messagebut1)      // gives popup message
+
+// but1[1].addEventListener("click", function messagebut2(){
+//     but1[0].removeEventListener("click",messagebut1)
+// }) 
+
+// const but1= document.querySelectorAll(".bclass")
+// but1[0].addEventListener("keydown",function(event){
+//     console.log(event.key)
+// })
