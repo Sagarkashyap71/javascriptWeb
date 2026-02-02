e1cf6ce8fcbe0ff73e21e61f3c318656a19c3b1e
Sagar <ssji2006@gmail.com>
2026-02-02 10:47:42 +0530

    callback

--- unit2.js
+++ unit2.js
@@ -45,8 +45,83 @@
 // }, 0);
 // console.log("Second line")
 
-console.log("First line")
-setTimeout(() => {                  // behaves as asyncronous 
-    console.log("After 2 sec")      // syntax : setTimeout(function,delay)
-}, 2000);
-console.log("Second line")
+// console.log("First line")
+// setTimeout(() => {                  // behaves as asyncronous 
+//     console.log("After 2 sec")      // syntax : setTimeout(function,delay)
+// }, 2000);
+// console.log("i didn't wait")
+
+
+//                        class 2 (2-2-2026)
+
+// callback function->
+
+// function print(name){                           // callback function = print
+//     console.log("hello students !",name)
+// }
+// function greet(num){                        // higher order function = greet
+//     console.log("welcome to my class !")
+//     setTimeout(() => {                      // here, settimieout is higher order function
+//         console.log("inside timeout")       // here, num function will wait for executing settimeout function
+//         let firstname= "Janish Jaat"
+//         num(firstname)
+
+//     }, 2000); 
+// }
+// greet(print)
+
+// callback hell
+
+// console.log("starting homework...")
+// setTimeout(() => {
+//     console.log("homework done")
+//     console.log("starting dinner...")
+//     setTimeout(() => {
+//         console.log("dinner done")
+//         console.log("getting ready to go out...")
+//         setTimeout(() => {
+//             console.log("going to playground!")
+//         }, 1000);    
+//     }, 1500);
+// }, 2000);
+ 
+
+// handling callback hell ->
+
+// function finishHomework(callback){
+//     console.log("Starting homework...")
+//     setTimeout(() => {
+//     console.log("homework done!")
+//     callback()
+//     }, 2000);
+// }
+// function eatDinner(callback){
+//     console.log("Starting dinner...")
+//     setTimeout(() => {
+//         console.log("dinner done!")
+//         callback()
+//     }, 1500);
+// }
+// function goToPlayground(){
+//     console.log("Going to the playground!")
+// }
+// finishHomework(()=>{
+//     eatDinner(()=>{
+//         goToPlayground()
+//     })
+// })
+
+
+// function print(name){                           // callback function = print
+//     console.log("hello students !",name)
+// }
+// function greet(num){                        // higher order function = greet
+//     console.log("welcome to my class !")
+//     setTimeout(() => {                      // here, settimieout is higher order function
+//         console.log("inside timeout")       // here, num function will wait for executing settimeout function
+//         let firstname= "Janish Jaat"
+//         num(firstname)
+
+//     }, 2000); 
+// }
+// greet(print())
